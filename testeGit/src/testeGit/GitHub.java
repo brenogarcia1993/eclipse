d0582b88494f01d95ba605a3ebf9058c50b56b3b
Breno Garcia <brenogarciamail@gmail.com>
2022-05-24 00:03:53 -0300

    atualizando linhas de código

--- testeGit/src/testeGit/GitHub.java
+++ testeGit/src/testeGit/GitHub.java
@@ -15,6 +15,8 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		System.out.println("Git");
+		System.out.println("Git");
+		System.out.println("Hub");
 
 	}
 
